3a211607853293fdbb5a3010db9942398240e22b
Sudnya Diamos <mailsudnya@gmail.com>
2014-01-15 22:19:13 -0800

    ADD: first java code on github

---
+++ src/TicTacToe.java
@@ -0,0 +1,10 @@
+public class TicTacToe {
+	public static void main(String[] args) {
+		   System.out.println("Hello World");
+		   System.out.println("1.     Start a new game.");
+		   System.out.println("2.     Quit a game.");
+		   System.out.println("3.     Select an opponent.");     
+		   System.out.println("4.     Complete a move.");     
+		   System.out.println("5.     Determine if there is a winner or if a game is not winnable.");
+		 }
+}
